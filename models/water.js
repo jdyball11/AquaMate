87ff36ea07af4cf82fa20147bb533585249d1f8b
jdyball11 <jack.dyball92@gmail.com>
2023-04-12 23:21:29 +1000

    added comments to models files

--- models/water.js
+++ models/water.js
@@ -5,6 +5,8 @@ const WaterSchema = new mongoose.Schema({
     
 })
 
+// don't uncomment below as this will create new model in mongo.
+
 // const Water = mongoose.model('Water', WaterSchema)
 
 module.exports = Meat
