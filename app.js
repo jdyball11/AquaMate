0da329146495b1f90ebd26eca55f874ea491b24e
jdyball11 <jack.dyball92@gmail.com>
2023-04-12 21:15:40 +1000

    connected to mongodb

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+// const mongoDBSession = require('connect-mongodb-session')
+const passport = require('passport')
+
+const app = express()
+const port = process.env.PORT
+const dbURL = process.env.MONGODB_URL
+
+mongoose.connect(dbURL)
+  .then(() => {
+    console.log('successfully connected to mongodb')
+  })
+  .catch((err) => {
+    console.log("error message", err)
+  });
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`)
+})
+
