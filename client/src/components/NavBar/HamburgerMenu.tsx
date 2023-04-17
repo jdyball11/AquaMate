495affac00043cace77f6ac1275ca3c294f382f9
tobias <tobias.bedford@insigniafinancial.com.au>
2023-04-17 20:46:56 +1000

    AM-004: added in x when hamburger open

--- client/src/components/NavBar/HamburgerMenu.tsx
+++ client/src/components/NavBar/HamburgerMenu.tsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
 import { RxHamburgerMenu } from 'react-icons/rx'
+import { RiCloseLine } from 'react-icons/ri'
+import styled from 'styled-components'
+
+const HamburgerMenuContainer = styled.div({
+})
+const HamburgerMenuButton = styled.button({
+    border: 'none',
+    backgroundColor: 'transparent'
+})
 
 const HamburgerMenu: React.FC = () => {
     const [open, setOpen] = useState(false);
+
     const handleClick = () => {
         setOpen(!open);
-        console.log("open: ", open)
     };
+
+    const icon = open ? <RiCloseLine /> : <RxHamburgerMenu />;
+
     return (
-        <div className="hamburger-menu">
-            <button className="hamburger-menu-button">
-                <RxHamburgerMenu onClick={handleClick} />
-            </button>
-        </div>
+        <HamburgerMenuContainer className="hamburger-menu">
+            <HamburgerMenuButton className="hamburger-menu-button" onClick={handleClick}>
+                {icon}
+            </HamburgerMenuButton>
+        </HamburgerMenuContainer>
     );
 };
 
