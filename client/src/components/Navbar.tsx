cb0c1492b8496d442594551979aa4e019ee044c2
jdyball11 <jack.dyball92@gmail.com>
2023-04-17 16:45:20 +1000

    removed unnecessary styling in app and index.css files. Added Home and Nav components

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const Navbar: React.FC = () => {
+
+
+  
+  return (
+    <div className="NavContainer">
+      <div className="NavLogo">Logo</div>
+      <div className="HamburgerContainer"></div>
+    </div>
+  );
+};
+
+export default Navbar;
